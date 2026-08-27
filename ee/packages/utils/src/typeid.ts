afdb84c5f3503147957095bebf647ba068bd0b80
Benjamin Shafii <ben@prologe.io>
2026-08-27 13:21:51 -0700

    feat(den): add remote session command protocol

--- ee/packages/utils/src/typeid.ts
+++ ee/packages/utils/src/typeid.ts
@@ -80,6 +80,7 @@ export const idTypesMapNameToPrefix = {
   automationRun: "atr",
   automationRunEvent: "ate",
   automationThread: "ath",
+  remoteSessionCommand: "rsc",
   codemodeRun: "cmr",
   workflowRun: "wfr",
   artifactView: "arv",
